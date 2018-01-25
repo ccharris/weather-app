1d7f9d7c924f26d8328b2dfe3194abe795762e54
Carolyn Harris <ccharris27@gmail.com>
2018-01-25 12:43:08 -0500

    updated styling

--- src/components/error.js
+++ src/components/error.js
@@ -3,18 +3,19 @@ import { connect } from 'react-redux';
 import styled from 'styled-components';
 
 const ErrorWrapper = styled.div`
-  background-color: white;
+  background-color: #ECE9E6;
   text-align: center;
   color: red;
   height: 20px;
   width: 100%;
-  display: ${props => (props.error == '') ? 'none' : 'inline'};
+  margin-top: 10px;
+  display: ${props => (props.error == '') ? 'none' : 'block'};
 `
 class Error extends Component {
     render () {
         return(
             <ErrorWrapper>
-                <p> {this.props.error} </p>
+             {this.props.error}
             </ErrorWrapper>
         );
     }   
